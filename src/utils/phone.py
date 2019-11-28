6ba457f28fb1d454e498ae551c68b271511d5b2a
ts <ts@appkode.ru>
2019-11-28 17:48:25 +0200

    add unnecessary function and test

--- src/utils/phone.py
+++ src/utils/phone.py
@@ -1,2 +1,7 @@
 def prepare_phone_number(phone_number: str) -> str:
-    pass
+    if phone_number.startswith('+7') and len(phone_number) == 12:
+        res = phone_number.replace('+7', '8')
+    else:
+        res = phone_number
+
+    return res
--- tests/utils/test_phone_utils.py
+++ tests/utils/test_phone_utils.py
@@ -3,4 +3,24 @@ from utils import phone
 
 class TestPhoneUtils:
     def test_phone_utils(self):
-        pass
+        phone_number = '+79113334455'
+        expected = '89113334455'
+
+        cases = [
+            dict(
+                phone_number='+79113334455',
+                expected='89113334455',
+            ),
+            dict(
+                phone_number='+79113',
+                expected='+79113',
+            ),
+            dict(
+                phone_number='+489113334455',
+                expected='+489113334455',
+            ),
+        ]
+
+        for case in cases:
+            assert phone.prepare_phone_number(case['phone_number']) == case['expected']
+
